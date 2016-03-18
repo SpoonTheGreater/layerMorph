f300a35be7291f0f9a6ea557b9cd43551e079717
James Sadlier <James@mobanode.com>
2016-03-18 17:32:09 +0000

    Initial Commit

---
+++ layerMorph/layerMorph/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  layerMorph
+//
+//  Created by James Sadlier on 18/03/2016.
+//  Copyright © 2016 SpoonWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
